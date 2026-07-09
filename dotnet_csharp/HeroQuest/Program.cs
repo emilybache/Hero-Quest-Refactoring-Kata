8aefe3284353aa36a888e3fea855de5ecf358546
emily <emily@bacheconsulting.com>
2026-07-09 15:16:33 +0200

    simple main function

---
+++ dotnet_csharp/HeroQuest/Program.cs
@@ -0,0 +1,20 @@
+using CodingDojo;
+using System.Text;
+
+HeroQuest.Output = new StringBuilder();
+
+QuestData questData = new ()
+{
+ PlayerName = "Conan",
+ PlayerHealth = 100,
+ PlayerStrength = 7,
+ PlayerMagic = 15,
+ PlayerCraftingSkill = 12,
+ ItemName = "Healing Potion",
+ ItemKind = "Health",
+ ItemPower = 20,
+ EnemyName = "Goblin Warlord",
+ EnemyPower = 12
+};
+
+Console.Out.WriteLine(HeroQuest.PlayerToString(questData));
